89be56f73ae4e40de354aeb0904a20bd9c3139d0
Pangoraw <naydex.mc+github@gmail.com>
2018-04-10 16:56:48 +0200

    [PB,HU,MR] Create a ListView

--- User/app/src/main/java/at/bytefactory/onestopreportuser/MainActivity.java
+++ User/app/src/main/java/at/bytefactory/onestopreportuser/MainActivity.java
@@ -1,7 +1,17 @@
 package at.bytefactory.onestopreportuser;
 
+import android.app.LauncherActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ListView listView = findViewById(R.id.list);
+        ArrayList<String> categories = new ArrayList<String>() {{
+            add("Lighting");
+            add("Trash");
+            add("Road");
+        }};
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, categories);
+        listView.setAdapter(adapter);
+        listView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
