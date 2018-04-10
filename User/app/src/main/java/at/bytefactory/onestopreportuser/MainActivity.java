1a7a889c7c12da59b8398d1e69e4ca8f90a5df59
Moritz Reis <reis@student.tugraz.at>
2018-04-10 18:58:48 +0200

    [PB, HU, MR] Add Buttons to list view

--- User/app/src/main/java/at/bytefactory/onestopreportuser/MainActivity.java
+++ User/app/src/main/java/at/bytefactory/onestopreportuser/MainActivity.java
@@ -1,10 +1,14 @@
 package at.bytefactory.onestopreportuser;
 
 import android.app.LauncherActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ExpandableListView;
@@ -17,9 +21,29 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnItemClickListener {
 
-    ArrayList<String> sections = new ArrayList<String>();
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.listView1);
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> l, View v, int position, long id) {
+        Log.i("HelloListView", "You clicked Item: " + id + " at position:" + position);
+        // Then you start a new Activity via Intent
+//        Intent intent = new Intent();
+//        intent.setClass(this, ListItemDetail.class);
+//        intent.putExtra("position", position);
+//        // Or / And
+//        intent.putExtra("id", id);
+//        startActivity(intent);
+    }
+    // ArrayList<String> sections = new ArrayList<String>();
 //    @Override
 //    protected void onCreate(Bundle savedInstanceState) {
 //        super.onCreate(savedInstanceState);
